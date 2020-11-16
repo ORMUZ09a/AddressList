11d6d0c53b5665ed9b35bdfbfea6db3c9755255b
Jose_Martinez <al0277334@tecmilenio.mx>
2020-11-16 17:44:44 -0600

    creating methods on AddressBook

---
+++ src/com/tecmilenio/ormuz/AddressBook.java
@@ -0,0 +1,24 @@
+package com.tecmilenio.ormuz;
+
+import java.util.HashMap;
+
+public class AddressBook {
+
+    private HashMap<String,String> contacts = new HashMap<>();
+
+    public void load(){
+        //Saves contacts of the file
+    }
+    public void save(){
+        //Saves chances of the file
+    }
+    public void list() {
+        //shows contacts od the address book
+    }
+    public void create() {
+        //creates new contact
+    }
+    public void delete() {
+        //delets a contact
+    }
+}
